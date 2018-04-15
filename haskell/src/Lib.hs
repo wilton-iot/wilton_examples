fb2bd6f05f7c6c8b33f7daf42f5dbf3ff596f080
alex <alex@staticlibs.net>
2018-04-15 16:42:41 +0100

    update haskell examle, add backcall to JS

--- haskell/src/Lib.hs
+++ haskell/src/Lib.hs
@@ -16,15 +16,21 @@
 
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Lib where
 
-import Data.Aeson
-import Data.Data
-import Data.Maybe
-import GHC.Generics
-import Foreign.C.String
-import Foreign.Wilton.FFI
+import Data.Maybe (isJust)
+import Data.Text (Text)
+import Foreign.C.String (CString)
+
+import qualified Data.Aeson as Aeson
+import qualified Data.ByteString.UTF8 as UTF8
+import qualified Data.Data as Data
+import qualified Data.HashMap.Strict as Map
+import qualified Data.Vector as Vector
+import qualified GHC.Generics as Generics
+import qualified Foreign.Wilton.FFI as WiltonFFI
 
 
 -- structs to be passed to/from JavaScript
@@ -32,23 +38,40 @@ import Foreign.Wilton.FFI
 data MyObjIn = MyObjIn {
     foo :: String,
     bar :: Int
-} deriving (Typeable, Data, Generic, Show)
-instance FromJSON MyObjIn
+} deriving (Data.Typeable, Data.Data, Generics.Generic, Show)
+instance Aeson.FromJSON MyObjIn
 
 data MyObjOut = MyObjOut {
     baz :: String,
     baa :: Int
-} deriving (Generic, Show)
-instance ToJSON MyObjOut
+} deriving (Generics.Generic, Show)
+instance Aeson.ToJSON MyObjOut
+
+-- backcall to JS
+
+capitalize :: Text -> IO String
+capitalize msg = do
+    let callDesc = Aeson.Object (Map.fromList [
+            ("module", "lodash/string"),
+            ("func", "capitalize"),
+            ("args", Aeson.Array (Vector.fromList [Aeson.String msg]))
+            ])
+    respEither <- WiltonFFI.runWiltonScript callDesc
+    either
+        (\err -> do
+            error err)
+        (\respBs -> do
+            return (UTF8.toString respBs))
+        respEither
 
 
 -- functions with some logic
 
 hello :: MyObjIn -> IO MyObjOut
 hello obj = do
-    putStrLn ("Input object: " ++ (show obj))
+    st <- capitalize "haskell"
     return MyObjOut {
-        baz = (foo obj ++ " from Haskell lib!"),
+        baz = (foo obj ++ " from " ++ st ++ " lib!"),
         baa = (bar obj + 1) }
 
 hello_exception :: MyObjIn -> IO MyObjOut
@@ -61,13 +84,13 @@ foreign export ccall wilton_module_init :: IO CString
 wilton_module_init :: IO CString
 wilton_module_init = do
     -- register a call for JavaScript
-    fooErr <- registerWiltoncall "example_hello" hello
+    fooErr <- WiltonFFI.registerWiltonCall "example_hello" hello
     -- check for error
-    if (isJust fooErr) then createWiltonError fooErr
+    if (isJust fooErr) then WiltonFFI.createWiltonError fooErr
     else do
         -- register some other call
-        barErr <- registerWiltoncall "example_hello_exception" hello_exception
+        barErr <- WiltonFFI.registerWiltonCall "example_hello_exception" hello_exception
         -- check for error
-        if (isJust barErr) then createWiltonError fooErr
+        if (isJust barErr) then WiltonFFI.createWiltonError fooErr
         -- return success
-        else createWiltonError Nothing
+        else WiltonFFI.createWiltonError Nothing
