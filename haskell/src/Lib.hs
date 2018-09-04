2bc819911bf54523a83b416a4ea9c63a47493ca1
alex <alex@staticlibs.net>
2018-09-04 19:00:29 +0100

    update ghc example

--- haskell/src/Lib.hs
+++ haskell/src/Lib.hs
@@ -14,65 +14,68 @@
 -- limitations under the License.
 --
 
-{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Lib where
 
+import Data.Aeson (FromJSON, ToJSON, (.=))
+import qualified Data.Aeson as Aeson
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as ByteString
+import qualified Data.ByteString.Lazy as ByteStringLazy
+import qualified Data.ByteString.UTF8 as UTF8
 import Data.Maybe (isJust)
 import Data.Text (Text)
+import Data.Typeable (Typeable)
 import Foreign.C.String (CString)
+import GHC.Generics (Generic)
+import Foreign.Wilton.FFI as WiltonFFI
 
-import qualified Data.Aeson as Aeson
-import qualified Data.ByteString.UTF8 as UTF8
-import qualified Data.Data as Data
-import qualified Data.HashMap.Strict as Map
-import qualified Data.Vector as Vector
-import qualified GHC.Generics as Generics
-import qualified Foreign.Wilton.FFI as WiltonFFI
 
+-- shortcut helper
+encodeBytes :: ToJSON a => a -> ByteString
+encodeBytes = ByteString.concat . ByteStringLazy.toChunks . Aeson.encode
 
 -- structs to be passed to/from JavaScript
 
-data MyObjIn = MyObjIn {
-    foo :: String,
-    bar :: Int
-} deriving (Data.Typeable, Data.Data, Generics.Generic, Show)
-instance Aeson.FromJSON MyObjIn
+data MyObjIn = MyObjIn
+    { foo :: String
+    , bar :: Int
+    } deriving (Generic, Show, Typeable)
+instance FromJSON MyObjIn
 
-data MyObjOut = MyObjOut {
-    baz :: String,
-    baa :: Int
-} deriving (Generics.Generic, Show)
-instance Aeson.ToJSON MyObjOut
+data MyObjOut = MyObjOut
+    { baz :: String
+    , baa :: Int
+    } deriving (Generic, Show)
+instance ToJSON MyObjOut
+
 
 -- backcall to JS
 
 capitalize :: Text -> IO String
 capitalize msg = do
-    let callDesc = Aeson.Object (Map.fromList [
-            ("module", "lodash/string"),
-            ("func", "capitalize"),
-            ("args", Aeson.Array (Vector.fromList [Aeson.String msg]))
-            ])
-    respEither <- WiltonFFI.runWiltonScript callDesc
-    either
-        (\err -> do
-            error err)
-        (\respBs -> do
-            return (UTF8.toString respBs))
-        respEither
-
+    -- use runscript_chakra on windows
+    -- bytestring version is used because result is a plain string
+    respEither <- invokeWiltonCallByteString "runscript_jsc" (encodeBytes (Aeson.object 
+        [ "module" .= ("lodash/string" :: Text)
+        , "func" .= ("capitalize" :: Text)
+        , "args" .= [msg]
+        ]))
+    case respEither of
+        Left err -> error (UTF8.toString err)
+        Right respBs -> return (UTF8.toString respBs)
 
 -- functions with some logic
 
 hello :: MyObjIn -> IO MyObjOut
 hello obj = do
     st <- capitalize "haskell"
-    return MyObjOut {
-        baz = (foo obj ++ " from " ++ st ++ " lib!"),
-        baa = (bar obj + 1) }
+    return MyObjOut
+        { baz = (foo obj ++ " from " ++ st ++ " lib!")
+        , baa = (bar obj + 1)
+        }
 
 hello_exception :: MyObjIn -> IO MyObjOut
 hello_exception = error "Error message from Haskell"
@@ -84,13 +87,13 @@ foreign export ccall wilton_module_init :: IO CString
 wilton_module_init :: IO CString
 wilton_module_init = do
     -- register a call for JavaScript
-    fooErr <- WiltonFFI.registerWiltonCall "example_hello" hello
+    fooErr <- registerWiltonCall "example_hello" hello
     -- check for error
-    if (isJust fooErr) then WiltonFFI.createWiltonError fooErr
+    if (isJust fooErr) then createWiltonError fooErr
     else do
         -- register some other call
-        barErr <- WiltonFFI.registerWiltonCall "example_hello_exception" hello_exception
+        barErr <- registerWiltonCall "example_hello_exception" hello_exception
         -- check for error
-        if (isJust barErr) then WiltonFFI.createWiltonError fooErr
+        if (isJust barErr) then createWiltonError fooErr
         -- return success
-        else WiltonFFI.createWiltonError Nothing
+        else createWiltonError Nothing
